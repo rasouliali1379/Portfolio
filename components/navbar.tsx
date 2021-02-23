b9a7f40d9bd8aeae6e265f0d5174b53d8f3c3b00
Ali Rasouli <samrd200046@gmail.com>
2021-02-23 12:23:19 +0330

    Initial Commit

---
+++ components/navbar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse asperiores
+      quibusdam facere delectus et, quisquam atque voluptatibus odio
+      consequuntur ducimus consectetur ea! Impedit culpa doloribus voluptatibus
+      repudiandae voluptatum, laborum commodi?
+    </p>
+  );
+};
+
+export default NavBar;
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import NavBar from '../components/navbar'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <NavBar/>
+    </div>
+  )
+}
